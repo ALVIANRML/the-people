a074da1e0bf39b7b54cf9bc51d10551e0b36615e
ALVIANRML <alvianrmli@gmail.com>
2025-01-29 00:15:39 +0700

    finish

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:5000/api";
-export const API_URL = "https://backend-the-people-afvj1p8p2-yannns-projects.vercel.app/api";
+export const API_URL = "https://backend-the-people-912turju6-yannns-projects.vercel.app/api";
